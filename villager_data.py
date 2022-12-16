027ca99ea6052ec06bad44185c04ed6593d327c0
Joshuah Ciafardone <joshuah.ciafardone@gmail.com>
2022-12-15 17:05:56 -0800

    Finished functions

--- villager_data.py
+++ villager_data.py
@@ -1,3 +1,5 @@
+#https://github.com/jciafardone/data-structure-lab.git
+
 """Functions to parse a file containing villager data."""
 
 
@@ -11,11 +13,18 @@ def all_species(filename):
         - set[str]: a set of strings
     """
 
+    opened_file = open(filename)
+    
     species = set()
+    
+    for line in opened_file:
+        villager = line.split("|")
+        species.add(villager[0])
 
-    # TODO: replace this with your code
-
+    #print(species)
     return species
+    
+all_species("villagers.csv")   
 
 
 def get_villagers_by_species(filename, search_string="All"):
@@ -29,11 +38,25 @@ def get_villagers_by_species(filename, search_string="All"):
         - list[str]: a list of names
     """
 
+    opened_file = open(filename)
+
     villagers = []
 
-    # TODO: replace this with your code
+    if search_string == "All":
+        for line in opened_file:
+            villager = line.split("|")
+            villagers.append(villager[0])
+    else:
+        for line in opened_file:
+            villager = line.split("|")
+            if villager[1] == search_string:
+                villagers.append(villager[0])
 
+    # TODO: replace this with your code
+    #print(sorted(villagers))
     return sorted(villagers)
+
+get_villagers_by_species("villagers.csv", search_string="All")
 
 
 def all_names_by_hobby(filename):
@@ -46,9 +69,28 @@ def all_names_by_hobby(filename):
         - list[list[str]]: a list of lists containing names
     """
 
-    # TODO: replace this with your code
+    hobbies = ["Fitness", "Nature", "Education", "Music", "Fashion", "Play"]
+    opened_file = open(filename)
 
-    return []
+    main_list = []
+    villagers = []
+
+    for line in opened_file:
+        villager = line.split("|")
+        villagers.append(villager)
+
+    for hobby in hobbies:
+        names = []
+        for villager in villagers:
+            if hobby == villager[3]:
+                names.append(villager[0])
+        main_list.append(names)
+    
+    # print(sorted(main_list[0]))
+    # print(sorted(main_list[1]))
+    return sorted(main_list)
+
+all_names_by_hobby("villagers.csv")
 
 
 def all_data(filename):
@@ -63,12 +105,16 @@ def all_data(filename):
     Return:
         - list[tuple[str]]: a list of tuples containing strings
     """
+    main_list = []
+    opened_file = open(filename)
+    for line in opened_file:
+        villager = line.split("|")
+        villager = tuple(villager)
+        main_list.append(villager)
 
-    all_data = []
-
-    # TODO: replace this with your code
-
-    return all_data
+    # print(main_list)
+    return main_list
+all_data("villagers.csv")
 
 
 def find_motto(filename, villager_name):
@@ -85,7 +131,22 @@ def find_motto(filename, villager_name):
         - str: the villager's motto or None
     """
 
-    # TODO: replace this with your code
+    opened_file = open(filename)
+
+    villagers = []
+
+    for line in opened_file:
+        villager = line.split("|")
+        villagers.append(villager)
+
+    for villager in villagers:
+        if villager_name == villager[0]:
+            #print(villager[4])
+            return(villager[4])
+    
+    return None
+
+find_motto("villagers.csv", "Johnny")
 
 
 def find_likeminded_villagers(filename, villager_name):
@@ -102,5 +163,27 @@ def find_likeminded_villagers(filename, villager_name):
         >>> find_likeminded_villagers('villagers.csv', 'Wendy')
         {'Bella', ..., 'Carmen'}
     """
+    opened_file = open(filename)
+    villagers = []
+    personality = None
+    likeminded = set()
 
-    # TODO: replace this with your code
+    for line in opened_file:
+        villager = line.split("|")
+        villagers.append(villager)
+    
+    for villager in villagers:
+        if villager[0] == villager_name:
+            personality = villager[2]
+    
+    for villager in villagers:
+        if villager[2] == personality:
+            likeminded.add(villager[0])
+    
+#find villager in list that is in the input parameter, then store their personality as its own variable
+#then iterate over list of lines looking for same personality 
+#add names to set "likeminded"
+    print(likeminded)
+    return(likeminded)
+find_likeminded_villagers("villagers.csv", "Cyrano")
+
